afda1966e2add9c8f8507ee107ea95242cb99c67
aslamahrahman <aslamahrahiman@gmail.com>
2019-12-07 10:22:21 -0500

    correct static link

--- GUI/gamestream/gamestream/urls.py
+++ GUI/gamestream/gamestream/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 from scoreboard.central import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('scoreboard/', include('scoreboard.urls')),
-]
+] 
 
 # Execute once
 udpReceiver = UDPBroadcastReceiver("192.168.1.3", 11000)
